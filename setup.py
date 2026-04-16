f26f67c2f7139e274415ca72dc38dfa0eeeba3a9
Jacob Hobbs <thaumkid@gmail.com>
2026-04-16 00:48:59 -0600

    updated black version to 26.3.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     package_data={},
     python_requires=">=3.10.0",
     zip_safe=False,
-    install_requires=["black==22.12.0"],
+    install_requires=["black==26.3.1"],
     extras_require={},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -53,6 +53,8 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
